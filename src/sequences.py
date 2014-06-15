625342181817a745c0983d11d340f2e5bbd7e243
Felipe Albrecht <felipe.albrecht@gmail.com>
2014-06-15 22:45:29 +0200

    Parallel sequences insertion

--- src/sequences.py
+++ src/sequences.py
@@ -1,19 +1,51 @@
 import gzip
 import os
+import itertools
 
-from settings import DATA_DIR, log
+from multiprocessing import Pool
 
-genome = "hg19"
+from client import EpidbClient
+from settings import DATA_DIR, DEEPBLUE_HOST, DEEPBLUE_PORT, max_threads
+from log import log
+
+def insert_sequence(t):
+	user_key = t[0]
+	seq_info = t[1]
+
+	print seq_info
+
+	log.info("Inserting %s for %s" %(seq_info["genome"], seq_info["chromosome"]))
+
+	sequence = gzip.open(seq_info["sequence_path"]).read()
+
+	epidb = EpidbClient(DEEPBLUE_HOST, DEEPBLUE_PORT)
+
+	print epidb.upload_chromosome(seq_info["genome"], seq_info["chromosome"], sequence, user_key)
+
 
 def insert_chromosome_sequences(epidb, genome, user_key):
-	genomes = DATA_DIR + "genomes/" + genome + "/"
+	seqs_dir = os.path.join(DATA_DIR, "genomes/", genome)
 
-	for file_name in os.listdir(genomes):
-		if file_name .endswith(".gz"):
+  	seq_infos = []
+
+	for file_name in os.listdir(seqs_dir):
+		if file_name.endswith(".gz"):
 			chromosome = file_name.split(".")[0]
-			f_full = genomes + file_name
-			sequence = gzip.open(f_full).read()
+			f_full = os.path.join(seqs_dir, file_name)
+						
+			seq_info = {}
+			seq_info["genome"] = genome
+			seq_info["chromosome"] = chromosome
+			seq_info["sequence_path"] = f_full
 
-		 	log.info("Inserting %s for %s" %(chromosome, genome) )
+			seq_infos.append(seq_info)
 
-			print epidb.upload_chromosome(genome, chromosome, sequence, user_key)
+	total = len(seq_infos)
+  	count = 0
+  	p = Pool(max_threads)
+  	log.info("Inserting Chromosome Sequence. Total of " + str(total) + " sequences.")    
+
+  	p.map(insert_sequence, itertools.izip(itertools.repeat(user_key), seq_infos))
+  
+  	p.close()
+  	p.join()
