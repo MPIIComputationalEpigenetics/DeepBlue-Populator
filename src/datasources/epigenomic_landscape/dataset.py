0aca886a38c2f4741b9c9269b710c4097fcd04a1
fiaxh <github@lightrise.org>
2015-02-22 17:33:46 +0100

    PEP8

--- src/datasources/epigenomic_landscape/dataset.py
+++ src/datasources/epigenomic_landscape/dataset.py
@@ -6,10 +6,11 @@ import gzip
 from log import log
 from epidb_interaction import PopulatorEpidbClient
 from dataset import Dataset
-from datasources.epigenomic_landscape.attribute_mapper import EpigenomicLandscapeAttributeMapper
+from datasources.epigenomic_landscape.attribute_mapper import \
+    EpigenomicLandscapeAttributeMapper
+
 
 class EpigenomicLandscapeDataset(Dataset):
-
     @property
     def download_path(self):
         return os.path.join(self.file_directory, self.file_name)
@@ -28,52 +29,52 @@ class EpigenomicLandscapeDataset(Dataset):
         if not os.path.exists(self.download_path):
             raise IOError(self.download_path)
 
-	localfile = ""
-	if self.type_ == "bedgraph":
-            	file_type = self.download_path.split(".")[-1]
-		print file_type
-            	if file_type == "gz":
-                	f = gzip.open(self.download_path, 'rb')  
-			file_content = f.read()
-			f.close()
-           		localfile = ".".join(self.download_path.split(".")[0:-1])
-			print localfile
-			f = open(localfile, "w")
-			f.write(file_content)
-			f.close()
-			extra_metadata['__local_file__'] = localfile
-        	else: 
-			extra_metadata['__local_file__'] = self.download_path
-		
-		file_content = ""
-		format = "bedgraph"
-	
-	elif self.type_ == 'bed':
-	        file_type = self.download_path.split(".")[-1]
-                if file_type == "gz":
-        		f = gzip.open(self.download_path, 'r')
-		else:
-        		f = open(self.download_path, 'r')
+        localfile = ""
+        if self.type_ == "bedgraph":
+            file_type = self.download_path.split(".")[-1]
+            print file_type
+            if file_type == "gz":
+                f = gzip.open(self.download_path, 'rb')
+                file_content = f.read()
+                f.close()
+                localfile = ".".join(self.download_path.split(".")[0:-1])
+                print localfile
+                f = open(localfile, "w")
+                f.write(file_content)
+                f.close()
+                extra_metadata['__local_file__'] = localfile
+            else:
+                extra_metadata['__local_file__'] = self.download_path
 
-        	file_content = f.readlines()
-        	f.close()
+            file_content = ""
+            format = "bedgraph"
 
-        	# remove lines with CHROMOSOME,START,END,STRAND="NA"	
-        	na_linecount = 0
-        	for line in list(file_content):
-            		if line.startswith("NA"):
-                		file_content.remove(line)
-                		na_linecount += 1
-			
-			if line.split()[0].endswith("_random"):
-				print  "remove random"
-				file_content.remove(line)
-				na_linecount += 1
+        elif self.type_ == 'bed':
+            file_type = self.download_path.split(".")[-1]
+            if file_type == "gz":
+                f = gzip.open(self.download_path, 'r')
+            else:
+                f = open(self.download_path, 'r')
 
-        	if na_linecount:
-            		log.info("NA lines removed: %d from %s", na_linecount, self.download_path)
+            file_content = f.readlines()
+            f.close()
 
-        	file_content = "".join(file_content)
+            # remove lines with CHROMOSOME,START,END,STRAND="NA"
+            na_linecount = 0
+            for line in list(file_content):
+                if line.startswith("NA"):
+                    file_content.remove(line)
+                    na_linecount += 1
+
+                if line.split()[0].endswith("_random"):
+                    print  "remove random"
+                    file_content.remove(line)
+                    na_linecount += 1
+
+            if na_linecount:
+                log.info("NA lines removed: %d from %s", na_linecount, self.download_path)
+
+            file_content = "".join(file_content)
 
         epidb = PopulatorEpidbClient()
 
@@ -84,8 +85,8 @@ class EpigenomicLandscapeDataset(Dataset):
             if status != "okay" or not len(samples_id):
                 log.critical("Sample for biosource %s was not found", am.biosource)
                 log.critical(samples_id)
-		if localfile:
-			os.remove(localfile)
+                if localfile:
+                    os.remove(localfile)
                 return
             sample_id = samples_id[0][0]
 
@@ -111,5 +112,5 @@ class EpigenomicLandscapeDataset(Dataset):
             self.save()
             log.info(msg)
 
-	if localfile:
-		os.remove(localfile)
+        if localfile:
+            os.remove(localfile)
--- src/populator.py
+++ src/populator.py
@@ -1,11 +1,10 @@
 import os
 import os.path
-import settings
 
+import settings
 import datasources.encode.vocabulary
 import column_definitions
 import repository_factory
-
 from annotations import insert_annotations
 from epidb_interaction import PopulatorEpidbClient
 from data_sources import project_sources
@@ -15,194 +14,213 @@ from owl_loader import load_owl
 from log import log
 from db import mdb
 
+
 """
 The Populator maintains the database connection and delegates
 tasks to its repositories and their datasets.
 """
+
+
 class Populator:
-  # epidb access data
-  username = settings.EPIDB_POPULATOR_USER[0]
-  email = settings.EPIDB_POPULATOR_USER[1]
-  institution = settings.EPIDB_POPULATOR_USER[2]
-  key = None
+    # epidb access data
+    username = settings.EPIDB_POPULATOR_USER[0]
+    email = settings.EPIDB_POPULATOR_USER[1]
+    institution = settings.EPIDB_POPULATOR_USER[2]
+    key = None
 
-  def __init__(self):
-    self.repositories = set([])
+    def __init__(self):
+        self.repositories = set([])
 
-    if os.path.exists(settings.EPIDB_AUTHKEY_FILE):
-      self.load_auth_info()
+        if os.path.exists(settings.EPIDB_AUTHKEY_FILE):
+            self.load_auth_info()
 
-  def __str__(self):
-    return "<Populator>"
+    def __str__(self):
+        return "<Populator>"
 
-  """
-  setup_epidb sets up access to epidb
-  """
-  def setup_epidb(self):
-    if os.path.exists(settings.EPIDB_AUTHKEY_FILE):
-      log.error("setup file %s for epidb already exists", settings.EPIDB_AUTHKEY_FILE)
-      # XXX: exit?
-      return False
+    """
+    setup_epidb sets up access to epidb
+    """
 
-    epidb = PopulatorEpidbClient()
-    res, admin_key = epidb.init_system(*settings.EPIDB_INIT_USER)
-    if (res == "error"):
-      log.error("error while initializing the system: %s", admin_key)
-      return False
+    def setup_epidb(self):
+        if os.path.exists(settings.EPIDB_AUTHKEY_FILE):
+            log.error("setup file %s for epidb already exists",
+                      settings.EPIDB_AUTHKEY_FILE)
+            # XXX: exit?
+            return False
 
-    log.info("admin user created successfully")
+        epidb = PopulatorEpidbClient()
+        res, admin_key = epidb.init_system(*settings.EPIDB_INIT_USER)
+        if res == "error":
+            log.error("error while initializing the system: %s", admin_key)
+            return False
 
-    res, u = epidb.add_user(self.username, self.email, self.institution)
-    if (res == "error"):
-      log.error("error while adding populator user: %s", u)
-      return False
+        log.info("admin user created successfully")
 
-    user_id, key = u
+        res, u = epidb.add_user(self.username, self.email, self.institution)
+        if (res == "error"):
+            log.error("error while adding populator user: %s", u)
+            return False
 
-    self.key = key
-    log.info("populator user created successfully")
+        user_id, key = u
 
-    with open(settings.EPIDB_AUTHKEY_FILE, 'w') as f:
-      f.write("%s:%s:%s:%s" % (settings.EPIDB_INIT_USER + (admin_key,)))
-      f.write('\n')
-      f.write("%s:%s:%s:%s" % (settings.EPIDB_POPULATOR_USER + (key,)))
+        self.key = key
+        log.info("populator user created successfully")
 
-    return True
+        with open(settings.EPIDB_AUTHKEY_FILE, 'w') as f:
+            f.write("%s:%s:%s:%s" % (settings.EPIDB_INIT_USER + (admin_key,)))
+            f.write('\n')
+            f.write("%s:%s:%s:%s" % (settings.EPIDB_POPULATOR_USER + (key,)))
 
-  """
-  load_auth_info loads a previously obtained authentication key for
-  epidb from the key file, defined in the settings.
-  """
-  def load_auth_info(self):
-    with open(settings.EPIDB_AUTHKEY_FILE, 'r') as f:
-      for l in f.readlines():
-        (user, email, inst, key) = l.split(':')
-        if (user, email, inst) == (self.username, self.email, self.institution):
-          self.key = key
-          break
+        return True
 
-    log.info("loaded authentication key successfully")
+    """
+    load_auth_info loads a previously obtained authentication key for
+    epidb from the key file, defined in the settings.
+    """
 
+    def load_auth_info(self):
+        with open(settings.EPIDB_AUTHKEY_FILE, 'r') as f:
+            for l in f.readlines():
+                (user, email, inst, key) = l.split(':')
+                if (user, email, inst) == (self.username, self.email, self.institution):
+                    self.key = key
+                    break
 
-  def insert_basic_data(self):
-    epidb = PopulatorEpidbClient()
+        log.info("loaded authentication key successfully")
 
-    self.insert_genomes()
-    self.insert_epigenetic_marks()
-    self.insert_technologies()
-    self.insert_projects()
+    def insert_basic_data(self):
 
-  def insert_genomes(self):
-    epidb = PopulatorEpidbClient()
+        self.insert_genomes()
+        self.insert_epigenetic_marks()
+        self.insert_technologies()
+        self.insert_projects()
 
-    epidb.add_genome("hg19", "Human Genome Assembly hg19", hg19_info)
-    epidb.add_genome("mm9", "Mouse Genome Assembly mm9", mm9_info)
-    epidb.add_genome("mm10", "Mouse Genome Assembly mm10", mm10_info)
-    #insert_chromosome_sequences(epidb, "hg19", self.key)
+    def insert_genomes(self):
+        epidb = PopulatorEpidbClient()
 
-
-  def insert_epigenetic_marks(self):
-    # TODO: enforce in the datasource the epigenetic mark
-    epidb = PopulatorEpidbClient()
-
-    epidb.add_epigenetic_mark("DNA Methylation", "DNA Methylation")
-    epidb.add_epigenetic_mark("DNaseI", "DNaseI hypersensitive sites")
-    epidb.add_epigenetic_mark("TFBS", "Transcription factor binding sites")
-    epidb.add_epigenetic_mark("Chromatin State Segmentation",  "A common set of states across the cell types were learned by computationally integrating ChIP-seq data for nine factors plus input using a Hidden Markov Model (HMM). In total, fifteen states were used to segment the genome.")
-    epidb.add_epigenetic_mark("mRNA-seq", "Messenger RNA")
-    epidb.add_epigenetic_mark("flRNA-seq", "Full length RNA")
-    epidb.add_epigenetic_mark("Input", "Experiment Input Data. It is not an epigenetic mark")
-    epidb.add_epigenetic_mark("Control", "Experiment Control Data. It is not an epigenetic mark")
-    epidb.add_epigenetic_mark("mRNA expression", "mRNA expression levels")
-
-    insert_histones(epidb, self.key)
-
-  def insert_technologies(self):
-    epidb = PopulatorEpidbClient()
-
-    epidb.add_technique("RRBS", "Reduced representation bisulfite sequencing", {})
-    epidb.add_technique("Infinium 450k", "Infinium HumanMethylation450", {})
-    epidb.add_technique("BisulfiteSeq", "Bisulfite sequencing or Bisulphite sequencing", {})
-    epidb.add_technique("ChIPseq", "ChIP-sequencing", {})
-    epidb.add_technique("ChIPseq Uniform", "ChIP-sequencing performed uniform processing on datasets produced by multiple data production groups in the ENCODE Consortium", {})
-    epidb.add_technique("DNaseSeq", "DNase I hypersensitive sites sequencing", {})
-    epidb.add_technique("DNaseSeq Uniform", "DNase I hypersensitive sites sequencing performed uniform processing on datasets produced by multiple data production groups in the ENCODE Consortium", {})
-    epidb.add_technique("Chromatin State Segmentation by HMM", "ChIP-seq data from the Broad Histone track was used to generate this track. Data for nine factors plus input and nine cell types was binarized separately at a 200 base pair resolution based on a Poisson background model. The chromatin states were learned from this binarized data using a multivariate Hidden Markov Model (HMM) that explicitly models the combinatorial patterns of observed modifications (Ernst and Kellis, 2010). To learn a common set of states across the nine cell types, first the genomes were concatenated across the cell types. For each of the nine cell types, each 200 base pair interval was then assigned to its most likely state under the model. Detailed information about the model parameters and state enrichments can be found in (Ernst et al, accepted).", {})
-    epidb.add_technique("RNASeq", "RNA sequencing", {})
-    epidb.add_technique("Microarray", "Various microarray techniques", {})
-    epidb.add_technique("Affymetrix Mouse Genome 430 2.0 Array", "", {})
-    epidb.add_technique("WGBS", "Whole-genome bisulfite sequencing", {})
+        epidb.add_genome("hg19", "Human Genome Assembly hg19", hg19_info)
+        epidb.add_genome("mm9", "Mouse Genome Assembly mm9", mm9_info)
+        epidb.add_genome("mm10", "Mouse Genome Assembly mm10", mm10_info)
+        # insert_chromosome_sequences(epidb, "hg19", self.key)
 
 
-  def insert_projects(self):
-    # TODO: Load these information from the source file.
-    # TODO: put in the source
-    epidb = PopulatorEpidbClient()
+    def insert_epigenetic_marks(self):
+        # TODO: enforce in the datasource the epigenetic mark
+        epidb = PopulatorEpidbClient()
 
-    epidb.add_project("ENCODE", "The ENCODE Project: ENCyclopedia Of DNA Elements")
-    epidb.add_project("Blueprint Epigenetics", "BLUEPRINT - A BLUEPRINT of Haematopoietic Epigenomes")
-    epidb.add_project("Mouse ENCODE", "The ENCODE Project: ENCyclopedia Of DNA Elements - Mouse")
-    epidb.add_project("epigenomic landscape - hematopoiesis - mouse - pilot", "")
-    epidb.add_project("DEEP", "DEEP - Deutsches Epigenom Programm")
+        epidb.add_epigenetic_mark("DNA Methylation", "DNA Methylation")
+        epidb.add_epigenetic_mark("DNaseI", "DNaseI hypersensitive sites")
+        epidb.add_epigenetic_mark("TFBS", "Transcription factor binding sites")
+        epidb.add_epigenetic_mark("Chromatin State Segmentation",
+                                  "A common set of states across the cell types were learned by computationally integrating ChIP-seq data for nine factors plus input using a Hidden Markov Model (HMM). In total, fifteen states were used to segment the genome.")
+        epidb.add_epigenetic_mark("mRNA-seq", "Messenger RNA")
+        epidb.add_epigenetic_mark("flRNA-seq", "Full length RNA")
+        epidb.add_epigenetic_mark("Input",
+                                  "Experiment Input Data. It is not an epigenetic mark")
+        epidb.add_epigenetic_mark("Control",
+                                  "Experiment Control Data. It is not an epigenetic mark")
+        epidb.add_epigenetic_mark("mRNA expression", "mRNA expression levels")
 
-  def create_columns(self):
-    epidb = PopulatorEpidbClient()
+        insert_histones(epidb, self.key)
 
-    for col in column_definitions.SIMPLE:
-      epidb.create_column_type_simple(*col)
-    for col in column_definitions.CATEGORY:
-      epidb.create_column_type_category(*col)
+    def insert_technologies(self):
+        epidb = PopulatorEpidbClient()
 
-    for col in column_definitions.RANGE:
-      epidb.create_column_type_range(*col)
-
-
-  def process_annotations(self):
-    insert_annotations(self.key)
-
-  def process_ontology(self):
-    load_owl(self.key)
-    datasources.encode.vocabulary.manual_curation(self.key)
-    datasources.encode.vocabulary.ensure_vocabulary(self.key)
-
-  """
-  setup_collections configures database internals for the Populator database
-  """
-  def setup_collections(self):
-    mdb.repositories.ensure_index([("path", 1)], unique=True)
-    mdb.datasets.ensure_index([("file_name", 1), ("repository_id", 1)], unique=True)
-
-  def load_repositories(self):
-    for sources in project_sources:
-      for (proj, genome, url) in sources:
-        r = repository_factory.load(proj, genome, url, self.key)
-        log.info("%s loaded", str(r))
-        self.repositories.add(r)
-        r.save()
-    log.info("populator initialized with %d repositories", len(self.repositories))
-
-  """
-  check_reposoitories reads all repositories and flags new datasets.
-  """
-  def check_repositories(self):
-    self.setup_collections()
-
-    for rep in self.repositories:
-      rep.read_datasets()
-
-    log.info("repositories checked successfully")
+        epidb.add_technique("RRBS", "Reduced representation bisulfite sequencing", {})
+        epidb.add_technique("Infinium 450k", "Infinium HumanMethylation450", {})
+        epidb.add_technique("BisulfiteSeq",
+                            "Bisulfite sequencing or Bisulphite sequencing", {})
+        epidb.add_technique("ChIPseq", "ChIP-sequencing", {})
+        epidb.add_technique("ChIPseq Uniform",
+                            "ChIP-sequencing performed uniform processing on datasets produced by multiple data production groups in the ENCODE Consortium",
+            {})
+        epidb.add_technique("DNaseSeq", "DNase I hypersensitive sites sequencing", {})
+        epidb.add_technique("DNaseSeq Uniform",
+                            "DNase I hypersensitive sites sequencing performed uniform processing on datasets produced by multiple data production groups in the ENCODE Consortium",
+            {})
+        epidb.add_technique("Chromatin State Segmentation by HMM",
+                            "ChIP-seq data from the Broad Histone track was used to generate this track. Data for nine factors plus input and nine cell types was binarized separately at a 200 base pair resolution based on a Poisson background model. The chromatin states were learned from this binarized data using a multivariate Hidden Markov Model (HMM) that explicitly models the combinatorial patterns of observed modifications (Ernst and Kellis, 2010). To learn a common set of states across the nine cell types, first the genomes were concatenated across the cell types. For each of the nine cell types, each 200 base pair interval was then assigned to its most likely state under the model. Detailed information about the model parameters and state enrichments can be found in (Ernst et al, accepted).",
+            {})
+        epidb.add_technique("RNASeq", "RNA sequencing", {})
+        epidb.add_technique("Microarray", "Various microarray techniques", {})
+        epidb.add_technique("Affymetrix Mouse Genome 430 2.0 Array", "", {})
+        epidb.add_technique("WGBS", "Whole-genome bisulfite sequencing", {})
 
 
-  """
-  process_repositories downloads all new datasets and inserts them
-  into epidb (processing).
-  Note: For this method to take any effect check_repositories must be
-  invoked beforehand.
-  """
-  def process_repositories(self):
-    log.info("processing repositories")
-    for rep in self.repositories:
-      rep.process_datasets(self.key)
+    def insert_projects(self):
+        # TODO: Load these information from the source file.
+        # TODO: put in the source
+        epidb = PopulatorEpidbClient()
 
-    log.info("repositories processed successfully")
+        epidb.add_project("ENCODE", "The ENCODE Project: ENCyclopedia Of DNA Elements")
+        epidb.add_project("Blueprint Epigenetics",
+                          "BLUEPRINT - A BLUEPRINT of Haematopoietic Epigenomes")
+        epidb.add_project("Mouse ENCODE",
+                          "The ENCODE Project: ENCyclopedia Of DNA Elements - Mouse")
+        epidb.add_project("epigenomic landscape - hematopoiesis - mouse - pilot", "")
+        epidb.add_project("DEEP", "DEEP - Deutsches Epigenom Programm")
+
+    def create_columns(self):
+        epidb = PopulatorEpidbClient()
+
+        for col in column_definitions.SIMPLE:
+            epidb.create_column_type_simple(*col)
+        for col in column_definitions.CATEGORY:
+            epidb.create_column_type_category(*col)
+
+        for col in column_definitions.RANGE:
+            epidb.create_column_type_range(*col)
+
+
+    def process_annotations(self):
+        insert_annotations(self.key)
+
+    def process_ontology(self):
+        load_owl(self.key)
+        datasources.encode.vocabulary.manual_curation(self.key)
+        datasources.encode.vocabulary.ensure_vocabulary(self.key)
+
+    """
+    setup_collections configures database internals for the Populator database
+    """
+
+    def setup_collections(self):
+        mdb.repositories.ensure_index([("path", 1)], unique=True)
+        mdb.datasets.ensure_index([("file_name", 1), ("repository_id", 1)], unique=True)
+
+    def load_repositories(self):
+        for sources in project_sources:
+            for (proj, genome, url) in sources:
+                r = repository_factory.load(proj, genome, url, self.key)
+                log.info("%s loaded", str(r))
+                self.repositories.add(r)
+                r.save()
+        log.info("populator initialized with %d repositories", len(self.repositories))
+
+    """
+    check_reposoitories reads all repositories and flags new datasets.
+    """
+
+    def check_repositories(self):
+        self.setup_collections()
+
+        for rep in self.repositories:
+            rep.read_datasets()
+
+        log.info("repositories checked successfully")
+
+
+    """
+    process_repositories downloads all new datasets and inserts them
+    into epidb (processing).
+    Note: For this method to take any effect check_repositories must be
+    invoked beforehand.
+    """
+
+    def process_repositories(self):
+        log.info("processing repositories")
+        for rep in self.repositories:
+            rep.process_datasets(self.key)
+
+        log.info("repositories processed successfully")
 
