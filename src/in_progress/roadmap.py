cffbd8950b25ee263188311df1dab49d1ad7cf40
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-04-20 19:19:37 +0200

    In Progress - Roadmap

---
+++ src/in_progress/roadmap.py
@@ -0,0 +1,89 @@
+from collections import defaultdict
+import csv
+from HTMLParser import HTMLParser
+import urllib2
+
+def parse_file_name(file_name):
+  print file_name
+  if not file_name.endswith("gz") and not file_name.endswith('bigwig'): return None
+  name, ext = file_name.split(".", 1)
+  print name
+  eid, emark = name.split("-", 1)
+  return eid, emark, file_name
+
+class MyHTMLParser(HTMLParser):
+  def __init__(self, root, experiments):
+    HTMLParser.__init__(self)
+    self.__root__ = root
+    self.__experiments__ = experiments
+
+  def handle_starttag(self, tag, attrs):
+    if tag == "a":
+      print tag, attrs
+      file = parse_file_name(attrs[0][1])
+      if file:
+        (eid, emark, file_name) = file
+        self.__experiments__[eid][emark] = self.__root__ + file_name
+
+experiments = defaultdict(lambda: defaultdict(str))
+
+def parse_data(root_address):
+  parser = MyHTMLParser(root_address, experiments)
+  f = urllib2.urlopen(root_address)
+  data = f.read()
+  print data
+  parser.feed(data)
+
+consolidated_narrow_peaks = "http://egg2.wustl.edu/roadmap/data/byFileType/peaks/consolidated/narrowPeak/"
+consolidated_broad_peaks = "http://egg2.wustl.edu/roadmap/data/byFileType/peaks/consolidated/broadPeak/"
+consolidated_signal = "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/" # -log10(p-value)
+consolidated_signal_fold_change = "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/foldChange/"
+fractional_methylation_wgbs = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/WGBS/FractionalMethylation_bigwig/"
+read_coverage_wgbs = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/WGBS/ReadCoverage_bigwig/"
+fractional_methylation_rrbs = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/RRBS/FractionalMethylation_bigwig/"
+read_coverage_bigwig_rrbs = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/RRBS/ReadCoverage_bigwig/"
+fractional_methylation_mcrf = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/mCRF/FractionalMethylation_bigwig/"
+
+
+sources = (consolidated_narrow_peaks, consolidated_broad_peaks, consolidated_signal, consolidated_signal_fold_change, fractional_methylation_wgbs, read_coverage_wgbs, fractional_methylation_rrbs, read_coverage_bigwig_rrbs, fractional_methylation_mcrf )
+
+for s in sources:
+  parse_data(s)
+
+import pprint
+
+pprint.pprint(dict(experiments))
+
+
+with open('jul2013.roadmapData.qc - Consolidated_EpigenomeIDs_summary_Table.csv', 'r') as csvfile:
+  reader = csv.DictReader(csvfile)
+  #print reader.fieldnames
+
+  keys = reader.fieldnames
+
+  new_keys = []
+  for k in keys:
+    if "\n" in k:
+      new_keys.append( k.split("\n")[0] )
+    else:
+      new_keys.append(k)
+
+  reader = csv.DictReader(csvfile, new_keys)
+
+  samples = []
+  ids = {}
+  reader.next() # header
+  reader.next() # sub header
+
+  for line in reader:
+    sample_info = {}
+
+    sample_keys = ["Comments", "Epigenome ID (EID)", "GROUP", "COLOR", "Epigenome Mnemonic", "Under Seq", "Quality Rating", "Auto Use Train (Core)", "Manual Use Train (Core)", "Train Core + K27ac", "Standardized Epigenome name", "Epigenome name (from EDACC Release 9 directory)", "ANATOMY", "TYPE", "LAB", "AGE", "SEX", "SOLID / LIQUID", "ETHNICITY", "Single Donor (SD) /Composite (C)", "DONOR / SAMPLE ALIAS", "CLASS"]
+
+    for key in sample_keys:
+      if line[key]:
+        sample_info[key] = line[key]
+
+    eid = sample_info["Epigenome ID (EID)"]
+    ids[eid] = sample_info
+    samples.append(sample_info)
