e66f3e65da6f9dcbbec8f2d7e080c03faf7669bf
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-04-20 19:39:34 +0200

    fixes

--- src/in_progress/roadmap.py
+++ src/in_progress/roadmap.py
@@ -3,47 +3,50 @@ import csv
 from HTMLParser import HTMLParser
 import urllib2
 
-def parse_file_name(file_name):
-  print file_name
+def parse_file_name(file_name, mark):
   if not file_name.endswith("gz") and not file_name.endswith('bigwig'): return None
   name, ext = file_name.split(".", 1)
-  print name
-  eid, emark = name.split("-", 1)
-  return eid, emark, file_name
+
+  if (mark == "methylation"):
+    eid, emark, ext = name.split("_", 2)
+    return eid, emark, file_name
+  else:
+    eid, emark= name.split("-", 1)
+    return eid, emark, file_name
 
 class MyHTMLParser(HTMLParser):
-  def __init__(self, root, experiments):
+  def __init__(self, root, mark, experiments):
     HTMLParser.__init__(self)
     self.__root__ = root
+    self.__mark__ = mark
     self.__experiments__ = experiments
 
   def handle_starttag(self, tag, attrs):
     if tag == "a":
-      print tag, attrs
-      file = parse_file_name(attrs[0][1])
+      file = parse_file_name(attrs[0][1], self.__mark__)
       if file:
         (eid, emark, file_name) = file
         self.__experiments__[eid][emark] = self.__root__ + file_name
 
 experiments = defaultdict(lambda: defaultdict(str))
 
-def parse_data(root_address):
-  parser = MyHTMLParser(root_address, experiments)
+def parse_data(link):
+  (root_address, mark) = link
+  parser = MyHTMLParser(root_address, mark, experiments)
   f = urllib2.urlopen(root_address)
   data = f.read()
   print data
   parser.feed(data)
 
-consolidated_narrow_peaks = "http://egg2.wustl.edu/roadmap/data/byFileType/peaks/consolidated/narrowPeak/"
-consolidated_broad_peaks = "http://egg2.wustl.edu/roadmap/data/byFileType/peaks/consolidated/broadPeak/"
-consolidated_signal = "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/" # -log10(p-value)
-consolidated_signal_fold_change = "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/foldChange/"
-fractional_methylation_wgbs = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/WGBS/FractionalMethylation_bigwig/"
-read_coverage_wgbs = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/WGBS/ReadCoverage_bigwig/"
-fractional_methylation_rrbs = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/RRBS/FractionalMethylation_bigwig/"
-read_coverage_bigwig_rrbs = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/RRBS/ReadCoverage_bigwig/"
-fractional_methylation_mcrf = "http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/mCRF/FractionalMethylation_bigwig/"
-
+consolidated_narrow_peaks = ("http://egg2.wustl.edu/roadmap/data/byFileType/peaks/consolidated/narrowPeak/",  "peak")
+consolidated_broad_peaks = ("http://egg2.wustl.edu/roadmap/data/byFileType/peaks/consolidated/broadPeak/", "peak")
+consolidated_signal = ("http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/", "peak") # -log10(p-value)
+consolidated_signal_fold_change = ("http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/foldChange/", "peak")
+fractional_methylation_wgbs = ("http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/WGBS/FractionalMethylation_bigwig/", "methylation")
+read_coverage_wgbs = ("http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/WGBS/ReadCoverage_bigwig/", "methylation")
+fractional_methylation_rrbs = ("http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/RRBS/FractionalMethylation_bigwig/", "methylation")
+read_coverage_bigwig_rrbs = ("http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/RRBS/ReadCoverage_bigwig/", "methylation")
+fractional_methylation_mcrf = ("http://egg2.wustl.edu/roadmap/data/byDataType/dnamethylation/mCRF/FractionalMethylation_bigwig/", "methylation")
 
 sources = (consolidated_narrow_peaks, consolidated_broad_peaks, consolidated_signal, consolidated_signal_fold_change, fractional_methylation_wgbs, read_coverage_wgbs, fractional_methylation_rrbs, read_coverage_bigwig_rrbs, fractional_methylation_mcrf )
 
