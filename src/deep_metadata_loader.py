7d78b812926e1d12c4bc9361cb05a7a072b2ce84
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-06-25 16:32:28 +0200

    copying DEEP files

--- src/deep_metadata_loader.py
+++ src/deep_metadata_loader.py
@@ -1,12 +1,19 @@
 import xmlrpclib
 import os
 import codecs
+import subprocess
 
 srv = xmlrpclib.Server('http://deep01.mpi-inf.mpg.de:50005')
 deepblue = xmlrpclib.Server("http://deepblue.mpi-inf.mpg.de/xmlrpc")
 user_key = "Ty3ArObJhuWK9MOy"
-print deepblue.echo(user_key)
 
+ssh_server = "infcontact1"
+ssh_user = "albrecht"
+ssh_download = "download/"
+
+file_types = ['signal', 'region']
+
+print deepblue.echo(user_key)
 
 samples = srv.get_files_by_type('Sample')
 
@@ -31,6 +38,9 @@ class Sample:
     else:
       print self._data
       print self._data['BIOMATERIAL_TYPE']
+
+  def data(self):
+    return self._data;
 
 def process_sample(file_location):
   data = {}
@@ -68,4 +78,21 @@ for s in samples[1]["p_iofiles_col"]:
   s, b = deepblue.is_biosource(bs, user_key)
   if (s == "error"):
     print bs
+  else:
+    deep_sample_id = sample.data()["DEEP_SAMPLE_ID"]
+
+    for type in file_types:
+      files = srv.get_files_by_type(type, deep_sample_id)
+      print files
+      for file_info in files[1]["p_iofiles_col"]:
+        #print file_info
+        file = file_info["filepath"] + file_info["filename"]
+
+        scp_command = "scp " + ssh_user+"@"+ssh_server+file+" "+ssh_download
+        print scp_command
+        subprocess.Popen(["scp", "%s@%s:%s" % (ssh_user, ssh_server, file), "%s" % (ssh_download)]).wait()
+
+
+
+
 
