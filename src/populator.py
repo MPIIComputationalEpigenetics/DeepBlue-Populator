21a0118e7b7dab83f7373e2fedc42a178fb73cfd
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-09-07 23:08:15 +0200

    Typo

--- src/populator.py
+++ src/populator.py
@@ -169,7 +169,7 @@ class Populator:
         epidb.add_technique("CAGE", "Cap Analysis of Gene Expression. http://www.osc.riken.jp/english/activity/cage/basic/", {})
         epidb.add_technique("transcription profiling by array assay", "", {})
         epidb.add_technique("single cell isolation followed by RNA-seq", "", {})
-        epidb.add_technique("RIP-seq", "RNA Immunoprecipitation followed by sequencing". {})
+        epidb.add_technique("RIP-seq", "RNA Immunoprecipitation followed by sequencing", {})
 
     def insert_projects(self):
         # TODO: Load these information from the source file.
